7ecf01cd5aae2dc18331c6128bf382a5d828fea1
iat <i@iat.net.cn>
2017-07-07 18:08:22 +0800

    增加群红包

--- src/TuLing.php
+++ src/TuLing.php
@@ -5,6 +5,9 @@ namespace Vbot\TuLing;
 use Vbot\Http\Http;
 use Hanson\Vbot\Message\Text;
 use Hanson\Vbot\Message\Emoticon;
+use Hanson\Vbot\Message\Image;
+use Hanson\Vbot\Message\Video;
+use Hanson\Vbot\Message\Voice;
 use Hanson\Vbot\Console\Console;
 use Illuminate\Support\Collection;
 use Hanson\Vbot\Extension\AbstractMessageHandler;
@@ -33,7 +36,7 @@ class TuLing extends AbstractMessageHandler
     private function autoReplyFriend($username, $message)
     {
         try {
-            if ($message['type'] == 'text') {
+            if ($message['type'] === 'text') {
                 if ($message['pure'] == '聊天') {
                     Text::send($username, '恭喜你解锁聊天功能, 你要聊什么呢?');
                     self::$users[$username]['tuling_id'] = $this->generateId();
@@ -44,8 +47,20 @@ class TuLing extends AbstractMessageHandler
                     Text::send($username, $this->tulingReply($message['pure'], self::$users[$username]['tuling_id']));
                 }
             }
-            if ($message['type'] == 'emoticon') {
+            if ($message['type'] === 'emoticon') {
                 Emoticon::sendRandom($username);
+            }
+            if ($message['type'] === 'recall') {
+                Text::send($username, $message['content'].' : '.$message['origin']['content']);
+                if ($message['origin']['type'] === 'image') {
+                    Image::send($username, $message['origin']);
+                } elseif ($message['origin']['type'] === 'emoticon') {
+                    Emoticon::send($username, $message['origin']);
+                } elseif ($message['origin']['type'] === 'video') {
+                    Video::send($username, $message['origin']);
+                } elseif ($message['origin']['type'] === 'voice') {
+                    Voice::send($username, $message['origin']);
+                }
             }
         } catch (\Exception $e) {
             vbot('console')->log($e->getMessage(), Console::ERROR);
@@ -56,8 +71,14 @@ class TuLing extends AbstractMessageHandler
     private function autoReplyGroup($username, $message)
     {
         try {
-            if (($message['type'] == 'text') && $message['isAt']) {
+            if (($message['type'] === 'text') && $message['isAt']) {
                 Text::send($username, $this->tulingReply($message['pure']));
+            }
+            if ($message['type'] === 'red_packet') {
+                $myself = vbot('myself');
+                $groupNickname = $message['from']['NickName'];
+                $senderNickname = $message['sender']['NickName'];
+                Text::send($myself->username, '[' . $groupNickname . ': '. $senderNickname . ']' . $message['content']);
             }
         } catch (\Exception $e) {
             vbot('console')->log($e->getMessage(), Console::ERROR);
